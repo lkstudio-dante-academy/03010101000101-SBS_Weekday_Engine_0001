c1d68a4a9537575d8fa871e0c5e38ea804f914c1
dante <lkstudio.dante@gmail.com>
2023-03-11 19:58:14 +0900

    no message

--- Programming/C#/Example/Training/Program.cs
+++ Programming/C#/Example/Training/Program.cs
@@ -7,10 +7,6 @@ using System.Threading.Tasks;
 namespace Training {
 	internal class Program {
 		static void Main(string[] args) {
-			//Classes.Training_01.CTraining_01.Start(args);
-			//Classes.Training_02.CTraining_02.Start(args);
-			Classes.Training_03.CTraining_03.Start(args);
-
 			Console.ReadKey();
 		}
 	}
