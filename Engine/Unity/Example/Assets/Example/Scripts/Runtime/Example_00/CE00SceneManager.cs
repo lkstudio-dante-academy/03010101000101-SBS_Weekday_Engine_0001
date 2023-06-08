1322acfadf67eb79756975a573933d1f5648fd81
dante <lkstudio.dante@gmail.com>
2023-06-08 23:17:16 +0900

    no message

--- Engine/Unity/Example/Assets/Example/Scripts/Runtime/Example_00/CE00SceneManager.cs
+++ Engine/Unity/Example/Assets/Example/Scripts/Runtime/Example_00/CE00SceneManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 /** Example 0 */
 public class CE00SceneManager : CSceneManager {
@@ -18,6 +20,31 @@ public class CE00SceneManager : CSceneManager {
 	/** 초기화 */
 	public override void Awake() {
 		base.Awake();
+
+		for(int i = 1; i < SceneManager.sceneCountInBuildSettings; ++i) {
+			int nIdx = i;
+			string oScenePath = SceneUtility.GetScenePathByBuildIndex(i);
+
+			// 버튼을 설정한다 {
+			var oMenuBtn = CFactory.CreateCloneGameObj<Button>("MenuBtn",
+				m_oOriginMenuBtn, m_oScrollViewContents, Vector3.zero, Vector3.one, Vector3.zero);
+
+			oMenuBtn.onClick.AddListener(() => this.OnTouchMenuBtn(nIdx));
+			// 버튼을 설정한다 }
+
+			// 텍스트 설정한다
+			var oMenuBtnText = oMenuBtn.GetComponent<Text>();
+			oMenuBtnText.text = Path.GetFileNameWithoutExtension(oScenePath);
+		}
+	}
+
+	/** 메뉴 버튼을 눌렀을 경우 */
+	private void OnTouchMenuBtn(int a_nIdx) {
+		/*
+		 * SceneManager 로 씬을 로드 할 때 씬 이름 대신에 씬 경로를 입력으로 넘겨줘도 전혀
+		 * 상관 없다는 것을 알 수 있다. (즉, 별도로 씬 경로로부터 씬 이름을 분리 할 필요가 없다.)
+		 */
+		CSceneLoader.Inst.LoadScene(SceneUtility.GetScenePathByBuildIndex(a_nIdx));
 	}
 	#endregion // 함수
 }
