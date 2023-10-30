66036c46cfde6180f79cce975bc230e3b23fd2f9
dante <lkstudio.dante@gmail.com>
2023-10-31 02:30:44 +0900

    no message

--- Engine/Unity/Example/Assets/Example/Scripts/Runtime/Global/Utility/Dispatcher/CEventDispatcher.cs
+++ Engine/Unity/Example/Assets/Example/Scripts/Runtime/Global/Utility/Dispatcher/CEventDispatcher.cs
@@ -6,12 +6,17 @@ using UnityEngine;
 public class CEventDispatcher : CComponent {
 	#region 프로퍼티
 	public System.Action<CEventDispatcher, string> EventCallback { get; set; } = null;
+	public System.Action<CEventDispatcher, GameObject> ParticleCallback { get; set; } = null;
 	#endregion // 프로퍼티
 
 	#region 함수
 	/** 이벤트를 수신했을 경우 */
 	public void OnReceiveEvent(string a_oEvent) {
 		this.EventCallback?.Invoke(this, a_oEvent);
+	}
+
+	public void OnParticleCollision(GameObject a_oGameObj) {
+		this.ParticleCallback?.Invoke(this, a_oGameObj);
 	}
 	#endregion // 함수
 }
